a209afeabf5ff1f1d2f4fd87e2ca77dc70529e70
Camille Vuillaume <camillevuillaume@gmail.com>
2026-01-06 23:00:41 +0900

    Working scripts for one liner and summary of the day

--- ascii-art.sh
+++ ascii-art.sh
@@ -1,230 +1,322 @@
 code_unknown=(
-    "    .-.      "
-    "     __)     "
-    "    (        "
-    "     \`-᾿     "
-    "      •      "
+	"    .-.      "
+	"     __)     "
+	"    (        "
+	"     \`-᾿     "
+	"      •      "
 )
 
 code_cloudy=(
-    "             "
-    $'\033[38;5;250m      .--.    \033[0m'
-    $'\033[38;5;250m  .-(    ).  \033[0m'
-    $'\033[38;5;250m (___.__)__) \033[0m'
-    "             "
+	"             "
+	$'\033[38;5;250m      .--.    \033[0m'
+	$'\033[38;5;250m  .-(    ).  \033[0m'
+	$'\033[38;5;250m (___.__)__) \033[0m'
+	"             "
 )
 
 code_fog=(
-    "             "
-    $'\033[38;5;251m _ - _ - _ - \033[0m'
-    $'\033[38;5;251m  _ - _ - _  \033[0m'
-    $'\033[38;5;251m _ - _ - _ - \033[0m'
-    "             "
+	"             "
+	$'\033[38;5;251m _ - _ - _ - \033[0m'
+	$'\033[38;5;251m  _ - _ - _  \033[0m'
+	$'\033[38;5;251m _ - _ - _ - \033[0m'
+	"             "
 )
 
 code_heavy_rain=(
-    $'\033[38;5;244;1m      .-.      \033[0m'
-    $'\033[38;5;244;1m     (   ).    \033[0m'
-    $'\033[38;5;244;1m    (___(__)   \033[0m'
-    $'\033[38;5;33;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m'
-    $'\033[38;5;33;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m'
+	$'\033[38;5;244;1m      .-.      \033[0m'
+	$'\033[38;5;244;1m     (   ).    \033[0m'
+	$'\033[38;5;244;1m    (___(__)   \033[0m'
+	$'\033[38;5;33;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m'
+	$'\033[38;5;33;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m'
 )
 
 code_heavy_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;244;1m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;244;1m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;244;1m(___(__) \033[0m'
-    $'\033[38;5;33;1m   ‚ʻ‚ʻ‚ʻ‚ʻ  \033[0m'
-    $'\033[38;5;33;1m   ‚ʻ‚ʻ‚ʻ‚ʻ  \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;244;1m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;244;1m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;244;1m(___(__) \033[0m'
+	$'\033[38;5;33;1m   ‚ʻ‚ʻ‚ʻ‚ʻ  \033[0m'
+	$'\033[38;5;33;1m   ‚ʻ‚ʻ‚ʻ‚ʻ  \033[0m'
 )
 
 code_heavy_snow=(
-    $'\033[38;5;244;1m      .-.      \033[0m'
-    $'\033[38;5;244;1m     (   ).    \033[0m'
-    $'\033[38;5;244;1m    (___(__)   \033[0m'
-    $'\033[38;5;255;1m   * * * * \033[0m'
-    $'\033[38;5;255;1m  * * * * \033[0m'
+	$'\033[38;5;244;1m      .-.      \033[0m'
+	$'\033[38;5;244;1m     (   ).    \033[0m'
+	$'\033[38;5;244;1m    (___(__)   \033[0m'
+	$'\033[38;5;255;1m   * * * * \033[0m'
+	$'\033[38;5;255;1m  * * * * \033[0m'
 )
 
 code_heavy_snow_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;244;1m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;244;1m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;244;1m(___(__) \033[0m'
-    $'\033[38;5;255;1m    * * * * \033[0m'
-    $'\033[38;5;255;1m   * * * * \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;244;1m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;244;1m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;244;1m(___(__) \033[0m'
+	$'\033[38;5;255;1m    * * * * \033[0m'
+	$'\033[38;5;255;1m   * * * * \033[0m'
 )
 
 code_light_rain=(
-    $'\033[38;5;250m      .-.      \033[0m'
-    $'\033[38;5;250m     (   ).    \033[0m'
-    $'\033[38;5;250m    (___(__)   \033[0m'
-    $'\033[38;5;111m    ʻ ʻ ʻ ʻ   \033[0m'
-    $'\033[38;5;111m   ʻ ʻ ʻ ʻ    \033[0m'
+	$'\033[38;5;250m      .-.      \033[0m'
+	$'\033[38;5;250m     (   ).    \033[0m'
+	$'\033[38;5;250m    (___(__)   \033[0m'
+	$'\033[38;5;111m    ʻ ʻ ʻ ʻ   \033[0m'
+	$'\033[38;5;111m   ʻ ʻ ʻ ʻ    \033[0m'
 )
 
 code_light_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    $'\033[38;5;111m      ʻ ʻ ʻ ʻ \033[0m'
-    $'\033[38;5;111m    ʻ ʻ ʻ ʻ  \033[0m'
+	$'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	$'\033[38;5;111m      ʻ ʻ ʻ ʻ \033[0m'
+	$'\033[38;5;111m    ʻ ʻ ʻ ʻ  \033[0m'
 )
 
 code_light_sleet=(
-    $'\033[38;5;250m      .-.      \033[0m'
-    $'\033[38;5;250m     (   ).    \033[0m'
-    $'\033[38;5;250m    (___(__)   \033[0m'
-    $'\033[38;5;111m    ʻ \033[38;5;255m*\033[38;5;111m ʻ \033[38;5;255m* \033[0m'
-    $'\033[38;5;255m   *\033[38;5;111m ʻ \033[38;5;255m*\033[38;5;111m ʻ    \033[0m'
+	$'\033[38;5;250m      .-.      \033[0m'
+	$'\033[38;5;250m     (   ).    \033[0m'
+	$'\033[38;5;250m    (___(__)   \033[0m'
+	$'\033[38;5;111m    ʻ \033[38;5;255m*\033[38;5;111m ʻ \033[38;5;255m* \033[0m'
+	$'\033[38;5;255m   *\033[38;5;111m ʻ \033[38;5;255m*\033[38;5;111m ʻ    \033[0m'
 )
 
 code_light_sleet_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    $'\033[38;5;111m      ʻ \033[38;5;255m*\033[38;5;111m ʻ \033[38;5;255m* \033[0m'
-    $'\033[38;5;255m    *\033[38;5;111m ʻ \033[38;5;255m*\033[38;5;111m ʻ  \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	$'\033[38;5;111m      ʻ \033[38;5;255m*\033[38;5;111m ʻ \033[38;5;255m* \033[0m'
+	$'\033[38;5;255m    *\033[38;5;111m ʻ \033[38;5;255m*\033[38;5;111m ʻ  \033[0m'
 )
 
 code_light_snow=(
-    $'\033[38;5;250m      .-.      \033[0m'
-    $'\033[38;5;250m     (   ).    \033[0m'
-    $'\033[38;5;250m    (___(__)   \033[0m'
-    $'\033[38;5;255m    * * * \033[0m'
-    $'\033[38;5;255m   * * * \033[0m'
+	$'\033[38;5;250m      .-.      \033[0m'
+	$'\033[38;5;250m     (   ).    \033[0m'
+	$'\033[38;5;250m    (___(__)   \033[0m'
+	$'\033[38;5;255m    * * * \033[0m'
+	$'\033[38;5;255m   * * * \033[0m'
 )
 
 code_light_snow_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    $'\033[38;5;255m      * * * \033[0m'
-    $'\033[38;5;255m    * * * \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	$'\033[38;5;255m      * * * \033[0m'
+	$'\033[38;5;255m    * * * \033[0m'
 )
 
 code_partly_cloudy=(
-    $'\033[38;5;226m   \\__/ \033[0m      '
-    $'\033[38;5;226m __/  \033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m   \\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    "             "
+	$'\033[38;5;226m   \\__/ \033[0m      '
+	$'\033[38;5;226m __/  \033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m   \\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	"             "
 )
 
 code_sunny=(
-    $'\033[38;5;226m    \\ . /    \033[0m'
-    $'\033[38;5;226m    - .-. -   \033[0m'
-    $'\033[38;5;226m  ‒ (   ) ‒  \033[0m'
-    $'\033[38;5;226m    . \`-᾿ .   \033[0m'
-    $'\033[38;5;226m     / \' \\    \033[0m'
+	$'\033[38;5;226m    \   /    \033[0m'
+	$'\033[38;5;226m     .-.     \033[0m'
+	$'\033[38;5;226m  ‒ (   ) ‒  \033[0m'
+	$'\033[38;5;226m     `-᾿     \033[0m'
+	$'\033[38;5;226m    /   \    \033[0m'
 )
 
 code_thundery_heavy_rain=(
-    $'\033[38;5;244;1m      .-.      \033[0m'
-    $'\033[38;5;244;1m     (   ).    \033[0m'
-    $'\033[38;5;244;1m    (___(__)   \033[0m'
-    $'\033[38;5;33;1m  ‚ʻ\033[38;5;228;5m⚡\033[38;5;33;25mʻ‚\033[38;5;228;5m⚡\033[38;5;33;25m‚ʻ   \033[0m'
-    $'\033[38;5;33;1m  ‚ʻ‚ʻ\033[38;5;228;5m⚡\033[38;5;33;25mʻ‚ʻ   \033[0m'
+	$'\033[38;5;244;1m      .-.      \033[0m'
+	$'\033[38;5;244;1m     (   ).    \033[0m'
+	$'\033[38;5;244;1m    (___(__)   \033[0m'
+	$'\033[38;5;33;1m  ‚ʻ\033[38;5;228;5m⚡\033[38;5;33;25mʻ‚\033[38;5;228;5m⚡\033[38;5;33;25m‚ʻ   \033[0m'
+	$'\033[38;5;33;1m  ‚ʻ‚ʻ\033[38;5;228;5m⚡\033[38;5;33;25mʻ‚ʻ   \033[0m'
 )
 
 code_thundery_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    $'\033[38;5;228;5m    ⚡\033[38;5;111;25mʻ ʻ\033[38;5;228;5m⚡\033[38;5;111;25mʻ ʻ \033[0m'
-    $'\033[38;5;111m    ʻ ʻ ʻ ʻ  \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	$'\033[38;5;228;5m    ⚡\033[38;5;111;25mʻ ʻ\033[38;5;228;5m⚡\033[38;5;111;25mʻ ʻ \033[0m'
+	$'\033[38;5;111m    ʻ ʻ ʻ ʻ  \033[0m'
 )
 
 code_thundery_snow_showers=(
-    $'\033[38;5;226m _\`/""\033[38;5;250m.-.    \033[0m'
-    $'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
-    $'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
-    $'\033[38;5;255m     *\033[38;5;228;5m⚡\033[38;5;255;25m *\033[38;5;228;5m⚡\033[38;5;255;25m * \033[0m'
-    $'\033[38;5;255m    * * * \033[0m'
+	$'\033[38;5;226m _`/""\033[38;5;250m.-.    \033[0m'
+	$'\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m'
+	$'\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m'
+	$'\033[38;5;255m     *\033[38;5;228;5m⚡\033[38;5;255;25m *\033[38;5;228;5m⚡\033[38;5;255;25m * \033[0m'
+	$'\033[38;5;255m    * * * \033[0m'
 )
 
-code_very_cloudy=(
-    "             "
-    $'\033[38;5;244;1m      .--.    \033[0m'
-    $'\033[38;5;244;1m  .-(    ).  \033[0m'
-    $'\033[38;5;244;1m (___.__)__) \033[0m'
-    "             "
-)
+get_weather_code() {
+	case $1 in
+	"clearsky") echo "code_sunny" ;;
+	"cloudy") echo "code_cloudy" ;;
+	"fair") echo "code_partly_cloudy" ;;
+	"partlycloudy") echo "code_partly_cloudy" ;;
+	"rainshowers") echo "code_light_showers" ;;
+	"rainshowersandthunder") echo "code_thundery_showers" ;;
+	"sleetshowers") echo "code_light_sleet_showers" ;;
+	"snowshowers") echo "code_light_snow_showers" ;;
+	"rain") echo "code_light_rain" ;;
+	"heavyrain") echo "code_heavy_rain" ;;
+	"heavyrainandthunder") echo "code_thundery_heavy_rain" ;;
+	"sleet") echo "code_light_sleet" ;;
+	"heavysleet") echo "code_light_sleet" ;;
+	"snow") echo "code_light_snow" ;;
+	"snowandthunder") echo "code_thundery_snow_showers" ;;
+	"fog") echo "code_fog" ;;
+	"sleetshowersandthunder") echo "code_thundery_snow_showers" ;;
+	"snowshowersandthunder") echo "code_thundery_snow_showers" ;;
+	"rainandthunder") echo "code_thundery_heavy_rain" ;;
+	"sleetandthunder") echo "code_thundery_snow_showers" ;;
+	"lightrainshowersandthunder") echo "code_thundery_showers" ;;
+	"heavyrainshowersandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightsleetshowersandthunder") echo "code_thundery_snow_showers" ;;
+	"heavysleetsshowersandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightsnowshowersandthunder") echo "code_thundery_snow_showers" ;;
+	"heavysnowshowersandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightrainandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightsleetandthunder") echo "code_thundery_snow_showers" ;;
+	"heavysleetandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightsnowandthunder") echo "code_thundery_snow_showers" ;;
+	"heavysnowandthunder") echo "code_thundery_heavy_rain" ;;
+	"lightrainshowers") echo "code_light_showers" ;;
+	"heavyrainshowers") echo "code_heavy_showers" ;;
+	"lightsleetshowers") echo "code_light_sleet_showers" ;;
+	"heavysleetshowers") echo "code_heavy_snow_showers" ;;
+	"lightsnowshowers") echo "code_light_snow_showers" ;;
+	"heavysnowshowers") echo "code_heavy_snow_showers" ;;
+	"lightrain") echo "code_light_rain" ;;
+	"lightsleet") echo "code_light_sleet" ;;
+	"lightsnow") echo "code_light_snow" ;;
+	"heavysnow") echo "code_heavy_snow" ;;
+	*) echo "code_unknown" ;;
+	esac
+}
 
-for line in "${code_unknown[@]}"; do
-    echo -e "$line"
-done
+get_weather_glyph() {
+	case $1 in
+	"clearsky") echo "☀️" ;;
+	"cloudy") echo "☁️" ;;
+	"fair") echo "⛅" ;;
+	"partlycloudy") echo "⛅" ;;
+	"rainshowers") echo "🌦️" ;;
+	"rainshowersandthunder") echo "⛈️" ;;
+	"sleetshowers") echo "🌨️" ;;
+	"snowshowers") echo "🌨️" ;;
+	"rain") echo "🌧️" ;;
+	"heavyrain") echo "🌧️" ;;
+	"heavyrainandthunder") echo "⛈️" ;;
+	"sleet") echo "🌨️" ;;
+	"heavysleet") echo "🌨️" ;;
+	"snow") echo "❄️" ;;
+	"snowandthunder") echo "🌨️⚡" ;;
+	"fog") echo "🌫️" ;;
+	"sleetshowersandthunder") echo "🌨️⚡" ;;
+	"snowshowersandthunder") echo "🌨️⚡" ;;
+	"rainandthunder") echo "⛈️" ;;
+	"sleetandthunder") echo "🌨️⚡" ;;
+	"lightrainshowersandthunder") echo "⛈️" ;;
+	"heavyrainshowersandthunder") echo "⛈️" ;;
+	"lightsleetshowersandthunder") echo "🌨️⚡" ;;
+	"heavysleetsshowersandthunder") echo "🌨️⚡" ;;
+	"lightsnowshowersandthunder") echo "🌨️⚡" ;;
+	"heavysnowshowersandthunder") echo "🌨️⚡" ;;
+	"lightrainandthunder") echo "⛈️" ;;
+	"lightsleetandthunder") echo "🌨️⚡" ;;
+	"heavysleetandthunder") echo "🌨️⚡" ;;
+	"lightsnowandthunder") echo "❄️⚡" ;;
+	"heavysnowandthunder") echo "❄️⚡" ;;
+	"lightrainshowers") echo "🌦️" ;;
+	"heavyrainshowers") echo "🌧️" ;;
+	"lightsleetshowers") echo "🌨️" ;;
+	"heavysleetshowers") echo "🌨️" ;;
+	"lightsnowshowers") echo "🌨️" ;;
+	"heavysnowshowers") echo "🌨️" ;;
+	"lightrain") echo "🌧️" ;;
+	"lightsleet") echo "🌨️" ;;
+	"lightsnow") echo "❄️" ;;
+	"heavysnow") echo "❄️" ;;
+	esac
+}
 
-for line in "${code_cloudy[@]}"; do
-    echo -e "$line"
-done
+print_ascii_art() {
+	for line in "${code_unknown[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_fog[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_cloudy[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_heavy_rain[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_fog[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_heavy_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_heavy_rain[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_heavy_snow[@]}"; do
-    echo -e "$line" 
-done
+	for line in "${code_heavy_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_heavy_snow_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_heavy_snow[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_rain[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_heavy_snow_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_rain[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_sleet[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_sleet_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_sleet[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_snow[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_sleet_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_light_snow_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_snow[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_partly_cloudy[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_light_snow_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_sunny[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_partly_cloudy[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_thundery_heavy_rain[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_sunny[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_thundery_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_thundery_heavy_rain[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_thundery_snow_showers[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_thundery_showers[@]}"; do
+		echo -e "$line"
+	done
 
-for line in "${code_very_cloudy[@]}"; do
-    echo -e "$line"
-done
+	for line in "${code_thundery_snow_showers[@]}"; do
+		echo -e "$line"
+	done
 
+	for line in "${code_very_cloudy[@]}"; do
+		echo -e "$line"
+	done
+}
 
-
+# get_weather_glyph "clearsky"
+# get_weather_glyph "heavyrainandthunder"
+# code_name=$(get_weather_code "snowshowersandthunder")
+# declare -n code_array="$code_name"
+# for line in "${code_array[@]}"; do
+#     echo -e "$line"
+# done
---
+++ weather.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+source ascii-art.sh
+CONFIG_FILE=$HOME/.config/wesh/wesh.conf
+CONFIG_DIR=$HOME/.config/wesh/
+WEATHER_DATA_FILE="$CONFIG_DIR/weather_data.json"
+SUN_DATA_FILE="$CONFIG_DIR/sun_data.json"
+
+#load config file
+load_config() {
+	CONFIG_FILE="$HOME/.config/wesh/wesh.conf"
+	if [[ -f "$CONFIG_FILE" ]]; then
+		source "$CONFIG_FILE"
+	else
+		echo "Config file not found!"
+	fi
+}
+
+# get location from ip address and write it to config file
+get_location() {
+	if [ ! -d $CONFIG_DIR ]; then
+		mkdir -p $CONFIG_DIR
+	fi
+	if [ ! -f $CONFIG_FILE ]; then
+		touch $CONFIG_FILE
+	fi
+
+	data=$(curl -s "https://ipapi.co/json")
+	lat=$(echo "$data" | jq -r '.latitude')
+	lon=$(echo "$data" | jq -r '.longitude')
+	echo "Detected location: Latitude=$lat, Longitude=$lon"
+	# Update or append LATITUDE
+	grep -q "^LATITUDE=" "$CONFIG_FILE" && sed -i "s/^LATITUDE=.*/LATITUDE=$lat/" "$CONFIG_FILE" || echo "LATITUDE=$lat" >>"$CONFIG_FILE"
+	# Update or append LONGITUDE
+	grep -q "^LONGITUDE=" "$CONFIG_FILE" && sed -i "s/^LONGITUDE=.*/LONGITUDE=$lon/" "$CONFIG_FILE" || echo "LONGITUDE=$lon" >>"$CONFIG_FILE"
+	source "$CONFIG_FILE"
+}
+
+# get weather and sun data from met.no API and store them in files
+get_weather() {
+	load_config
+	# get location if not set
+	if [[ -z "$LATITUDE" || -z "$LONGITUDE" ]]; then
+		get_location
+	fi
+
+	# check if data is older than 1 hour or files do not exist
+	timestamp=$(date +%s)
+	if [[ -z "$TIMESTAMP" || $((timestamp - TIMESTAMP)) -gt 3600 || ! -f $WEATHER_DATA_FILE || ! -f $SUN_DATA_FILE ]]; then
+		# Update or append TIMESTAMP
+		grep -q "^TIMESTAMP=" "$CONFIG_FILE" && sed -i "s/^TIMESTAMP=.*/TIMESTAMP=$timestamp/" "$CONFIG_FILE" || echo "TIMESTAMP=$timestamp" >>"$CONFIG_FILE"
+		#get weather data
+		weather=$(curl -A "wesh (https://github.com/camillevuillaume)" -X GET --header "Accept: application.json" "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=$LATITUDE&lon=$LONGITUDE")
+
+		#get sun data with timezone offset
+		offset_encoded=$(date +%:z | sed 's/+/%2B/g; s/:/%3A/g')
+		sun_data=$(curl -A "wesh (https://github.com/camillevuillaume)" -X GET --header "Accept: application.json" "https://api.met.no/weatherapi/sunrise/3.0/sun?lat=$LATITUDE&lon=$LONGITUDE&offset=$offset_encoded")
+
+		# Write weather data to file
+		echo "$weather" >"$WEATHER_DATA_FILE"
+		echo "$sun_data" >"$SUN_DATA_FILE"
+	fi
+}
+
+print_weather_bar() {
+	get_weather
+	weather_data=$(cat "$WEATHER_DATA_FILE")
+	temperature=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."air_temperature"')
+	temperature=$(printf "%.0f" "$temperature")
+	weather_code=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_1_hours.summary.symbol_code' | cut -d'_' -f1)
+	echo "🌡️ ${temperature}°C | $(get_weather_glyph $weather_code)"
+}
+
+print_weather_short() {
+	get_weather
+	weather_data=$(cat "$WEATHER_DATA_FILE")
+	sun_data=$(cat "$SUN_DATA_FILE")
+	temperature=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."air_temperature"')
+  humidity=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."relative_humidity"')
+	weather_code=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_1_hours.summary.symbol_code' | cut -d'_' -f1)
+	wind_speed=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."wind_speed"')
+	sunrise=$(echo "$sun_data" | jq -r '.properties.sunrise.time' | cut -d'+' -f1 | cut -d'T' -f2)
+	sunset=$(echo "$sun_data" | jq -r '.properties.sunset.time' | cut -d'+' -f1 | cut -d'T' -f2)
+	TODAY=$(date +%Y-%m-%d)
+	max_temp=$(echo "$weather_data" | jq -r --arg today "$TODAY" '[.properties.timeseries[] | select((.time | fromdateiso8601 | strftime("%Y-%m-%d")) == $today) | .data.instant.details.air_temperature] | max // 0')
+	min_temp=$(echo "$weather_data" | jq -r --arg today "$TODAY" '[.properties.timeseries[] | select((.time | fromdateiso8601 | strftime("%Y-%m-%d")) == $today) | .data.instant.details.air_temperature] | min // 0')
+  max_wind=$(echo "$weather_data" | jq -r --arg today "$TODAY" '[.properties.timeseries[] | select((.time | fromdateiso8601 | strftime("%Y-%m-%d")) == $today) | .data.instant.details.wind_speed] | max // 0')
+
+  code_name=$(get_weather_code "$weather_code")
+  declare -n code_array="$code_name"
+  echo "${code_array[0]} | 📅 Date: $TODAY"
+  echo "${code_array[1]} | 🌡️ ${temperature}°C (H: ${max_temp}°C L: ${min_temp}°C)"
+  echo "${code_array[2]} | 🌬️ ${wind_speed} m/s (H: ${max_wind} m/s)"
+  echo "${code_array[3]} | ☀️ $(date -d $sunrise +%H:%M) 🌙 $(date -d $sunset +%H:%M)"
+  echo "${code_array[4]} | 💧 ${humidity}%"
+
+}
+
+print_weather_bar
+print_weather_short
