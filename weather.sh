b7050c6f04486a6208a351c7db56ff46f4ec6262
Camille Vuillaume <camillevuillaume@gmail.com>
2026-01-07 08:30:14 +0900

    clean implementation for weather today

--- weather.sh
+++ weather.sh
@@ -68,7 +68,7 @@ print_weather_bar() {
 	temperature=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."air_temperature"')
 	temperature=$(printf "%.0f" "$temperature")
 	weather_code=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_1_hours.summary.symbol_code' | cut -d'_' -f1)
-	echo "🌡️ ${temperature}°C | $(get_weather_glyph $weather_code)"
+	echo "🌡️${temperature}°C $(get_weather_glyph $weather_code)"
 }
 
 print_weather_short() {
@@ -79,6 +79,8 @@ print_weather_short() {
   humidity=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."relative_humidity"')
 	weather_code=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_1_hours.summary.symbol_code' | cut -d'_' -f1)
 	wind_speed=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.instant.details."wind_speed"')
+  precipitation=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_1_hours.details."precipitation_amount"')
+  precipitation_6=$(echo "$weather_data" | jq -r '.properties.timeseries[0].data.next_6_hours.details."precipitation_amount"')
 	sunrise=$(echo "$sun_data" | jq -r '.properties.sunrise.time' | cut -d'+' -f1 | cut -d'T' -f2)
 	sunset=$(echo "$sun_data" | jq -r '.properties.sunset.time' | cut -d'+' -f1 | cut -d'T' -f2)
 	TODAY=$(date +%Y-%m-%d)
@@ -88,12 +90,13 @@ print_weather_short() {
 
   code_name=$(get_weather_code "$weather_code")
   declare -n code_array="$code_name"
-  echo "${code_array[0]} | 📅 Date: $TODAY"
-  echo "${code_array[1]} | 🌡️ ${temperature}°C (H: ${max_temp}°C L: ${min_temp}°C)"
-  echo "${code_array[2]} | 🌬️ ${wind_speed} m/s (H: ${max_wind} m/s)"
-  echo "${code_array[3]} | ☀️ $(date -d $sunrise +%H:%M) 🌙 $(date -d $sunset +%H:%M)"
-  echo "${code_array[4]} | 💧 ${humidity}%"
-
+  echo "📅 $TODAY |"
+  echo "--------------┼--------------------------------"
+  echo "${code_array[0]} | 🌡️ ${temperature}°C (H: ${max_temp}°C L: ${min_temp}°C)"
+  echo "${code_array[1]} | 🌬️ ${wind_speed} m/s (H: ${max_wind} m/s)"
+  echo "${code_array[2]} | ☀️ $(date -d $sunrise +%H:%M) 🌙 $(date -d $sunset +%H:%M)"
+  echo "${code_array[3]} | 🌧️ ${precipitation}mm (next 6 hours ${precipitation_6}mm)"
+  echo "${code_array[4]} | 💧 humidity ${humidity}%"
 }
 
 print_weather_bar
